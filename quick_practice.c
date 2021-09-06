2ad4f5889bb43bb28085f0f30ccaa121ddb153ce
Raj Joshi <43139255+rajjoshi96@users.noreply.github.com>
2021-09-06 18:34:12 +0800

    test commit

--- quick_practice.c
+++ quick_practice.c
@@ -1,71 +1,4 @@
 #include<stdio.h>
-// int swap(int *a,int *b){
-//     int t;
-//     t = *a;
-//     *a = *b;
-//     *b = t;
-// }
-// int pivot_last(int array[], int low, int high) {
-// 	int pivot = array[high];
-// 	int i = (low - 1);
- 
-// 	for (int j = low; j < high; j++) {
-// 		if (array[j] < pivot) {
-// 		    swap(&array[++i], &array[j]);
-// 		}
-// 	}
- 
-// 	swap(&array[i + 1], &array[high]);
-// 	return (i + 1);
-// }
-
-// int pivot_median(int arra[],int low,int high){
-//     int pivot;
-//     int mid = (high + low)/ 2;
-//     if(arra[mid] < arra[low])
-//     {
-//         swap(&arra[mid],&arra[low]);
-//     }
-//     if(arra[high] < arra[low])
-//     {
-//         swap(&arra[high], &arra[low]);
-//     }
-//     if(arra[high]< arra[mid])
-//     {
-//         swap(&arra[mid],&arra[high]);
-//     }
-//     swap(&arra[mid],&arra[high-1]);
-//     pivot = arra[high-1];
-//     return pivot_last(arra, low,high);
-// }
-// int quick(int a[], int low, int high){
-//     if(low< high)
-//     {
-//         int pi = pivot_median(a , low, high);
-//         pivot_median(a ,low,pi-1);
-//         pivot_median(a, pi + 1, high);
-//     }
-// }
-// int main()
-// {
-//     int arr[]={12,56,78,45,999,414,333,2,8558,38,98,9434313};
-//     int size = sizeof(arr)/sizeof(arr[0]);
-//     printf("Before Sorting array: \n");
-//     for(int i = 0;i < size;i++){
-//         printf("%d \t", arr[i]);
-//     }
-//     printf("\n");
-//     quick(arr,0,size-1);
-//     printf("After quick sort with Median \n");
-//     for(int i=0;i<size;i++)
-//     {
-//         printf("%d \t", arr[i]);
-//     }
-//     getchar();
-//     return 0;
-// }
-
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -90,29 +23,6 @@ int partition_pivot_last(int array[], int low, int high) {
 	return (i + 1);
 }
  
-// int partition_pivot_first(int array[], int low, int high) {
-// 	int pivot = array[low];
-// 	int i = (low + 1);
- 
-// 	for (int j = low + 1; j <= high; j++) {
-// 		if (array[j] < pivot) {
-// 		if (j != i) {
-// 		swap(&array[i], &array[j]);
-// 		}
-// 		i++;
-// 		}
-// 	}
- 
-// 	swap(&array[i - 1], &array[low]);
-// 	return (i - 1);
-// }
- 
-// int partition_pivot_random(int array[], int low, int high) {
-// 	int pivot;
-// 	int n = rand();
-// 	pivot = low + n % (high - low + 1); // Randomizing the pivot
-// 	return partition_pivot_last(array, low, high);
-// }
  
 int partition_pivot_median(int array[], int low, int high) {
 	
@@ -157,7 +67,7 @@ int main(void) {
  
 	printf("\n");
 	for (int i = 0; i < size; i++)
-		printf("|%d \t", array[i]);
+		printf("|%d \n", array[i]);
  
 	//getchar();
 	return 0;
